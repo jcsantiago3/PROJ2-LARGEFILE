80c365dcbdaf22d8553f6b1cf5e854cf1a0b9114
JC <jc.santiago0312@gmail.com>
2018-03-22 06:47:54 +0800

    Building the about.php

--- Website/about.php
+++ Website/about.php
@@ -6,7 +6,36 @@
     <body class="bgcolor">
         <?php include "nav.php"; ?>
         <div class="content contentpage contentwidth">
-
+            <div class="panel panel-default">
+                <div class="panel-header">
+                    <h2>About the Project</h2>
+                </div>
+                <div class="panel-body">
+                    <p>Test</p>
+                </div>
+                <div class="panel-footer">
+                    <p>Test</p>
+                </div>
+            </div>
+            <div class="panel panel-default">
+                <div class="panel-header">
+                    <h2>About Me</h2>
+                </div>
+                <div class="panel-body">
+                    <p>Test</p>
+                </div>
+                <div class="panel-footer">
+                    <p>Test</p>
+                </div>
+            </div>
+            <div class="panel panel-default">
+                <div class="panel-header">
+                    <h2>Sources</h2>
+                </div>
+                <div class="panel-body">
+                    <p>Test</p>
+                </div>
+            </div>
         </div>
         <?php include "footer.php"; ?>
     </body>
