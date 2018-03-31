6deed90c2dc6dfd33e05113c75f26f4471b2c9ee
Jehseh <jc.santiago0312@gmail.com>
2018-03-31 18:34:13 +0800

    added Disqus

--- Website/games/page_sodes.php
+++ Website/games/page_sodes.php
@@ -17,10 +17,30 @@
                 <div class="devbanner sodes"></div>
                 <?php include "../page-headers/page-header_sodes.php";
                 ?>
-            </div>
-            <hr>
-            <div>
-                <h2 class="whitetext">Reviews</h2>
+                <div class="panel panel-default">
+                    <div class="panel-header"><h2>Reviews</h2></div>
+                    <div class="panel-body">
+                        <div id="disqus_thread"></div>
+                        <script>
+                        /**
+                        *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+                        *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+                        /*
+                        var disqus_config = function () {
+                        this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+                        this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+                        };
+                        */
+                        (function() { // DON'T EDIT BELOW THIS LINE
+                        var d = document, s = d.createElement('script');
+                        s.src = 'https://devsstory.disqus.com/embed.js';
+                        s.setAttribute('data-timestamp', +new Date());
+                        (d.head || d.body).appendChild(s);
+                        })();
+                        </script>
+                        <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+                    </div>
+                </div>
             </div>
         </div>
         <?php include "../footer.php" ?>
