c895979d6b7e32a5a4b1c307f4f7a1777e8a8355
Jehseh <jc.santiago0312@gmail.com>
2018-04-03 15:21:17 +0800

    added documents

--- Website/footer.php
+++ Website/footer.php
@@ -1,4 +1,4 @@
 <!--otherpagefooter-->
 <div class="footerXD">
-    <div class="midtext whitetext footertext"> &copy; JC Santiago 2018, All Rights Reserved. </div>
+    <div class="midtext whitetext footertext"> &copy; JC Santiago 2018. All images and references are for educational purposes only. All rights reserved.</div>
 </div>
